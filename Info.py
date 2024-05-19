64e0344ccd30e930f29897f0a22c836d2f0ac70a
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:30:59 -0400

    Update Info.py

--- Info.py
+++ Info.py
@@ -1,4 +1,4 @@
-HtmlFile = open("hold.html", ‘r’, encoding=‘utf-8’)
+HtmlFile = open("hold.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 print(source_code)
 components.html(source_code, height=600)
