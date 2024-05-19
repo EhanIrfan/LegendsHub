fd4b4edc5a58e38370bf2afde37c75327581793f
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:42:12 -0400

    Update Guess Who.py

--- pages/Guess Who.py
+++ pages/Guess Who.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
-print(source_code)
-components.html(source_code, height=600)
+# source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
+# print(source_code)
+# components.html(source_code, height=600)
 
 import random
 from typing import Any
