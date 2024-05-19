72c15f4708840b193858aa75edffda4aa664b5ab
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:28:21 -0400

    Update Info.py

--- Info.py
+++ Info.py
@@ -1,9 +1,9 @@
-import streamlit as st
-
 HtmlFile = open(“hold.html”, ‘r’, encoding=‘utf-8’)
 source_code = HtmlFile.read()
 print(source_code)
 components.html(source_code, height=600)
+
+import streamlit as st
 
 st.set_page_config(
     page_title="DBL"
