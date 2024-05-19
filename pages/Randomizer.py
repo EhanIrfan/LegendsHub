ec7491866e37de29f162782193d322465f6aab91
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:38:52 -0400

    Update Randomizer.py

--- pages/Randomizer.py
+++ pages/Randomizer.py
@@ -6,8 +6,8 @@ st.set_page_config(page_title="Legends Randomizer", layout="wide")
 
 
 
-source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567"
-     crossorigin="anonymous"></script>'
+
+source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
 print(source_code)
 components.html(source_code, height=600)
 
