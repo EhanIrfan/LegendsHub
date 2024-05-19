553019dc8a59cb148529d79fc32ed2e59fcbc3f9
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:39:57 -0400

    Update Guess Who.py

--- pages/Guess Who.py
+++ pages/Guess Who.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Solo Guess Who", layout="wide")
-
-
 source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
 print(source_code)
 components.html(source_code, height=600)
