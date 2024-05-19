c6ca1f26e17218bb537af1fbd67f54520b416ea6
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:50:45 -0400

    Update Guess Who.py

--- pages/Guess Who.py
+++ pages/Guess Who.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
-# print(source_code)
-# components.html(source_code, height=600)
-
 import random
 from typing import Any
 
