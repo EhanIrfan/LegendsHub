83125715cf497e9c3f3f4c5b14da23be0a9c75d0
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:38:39 -0400

    Update Info.py

--- Info.py
+++ Info.py
@@ -5,8 +5,7 @@ st.set_page_config(
     page_title="DBL"
 )
 
-source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567"
-     crossorigin="anonymous"></script>'
+source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
 print(source_code)
 components.html(source_code, height=600)
 
