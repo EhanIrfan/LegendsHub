7617821b87a3a2b1efa39d60925e2601e61b93da
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:34:03 -0400

    Update Guess Who.py

--- pages/Guess Who.py
+++ pages/Guess Who.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-HtmlFile = open("test.html", 'r', encoding='utf-8')
+HtmlFile = open("hold.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 print(source_code)
 components.html(source_code, height=600)
