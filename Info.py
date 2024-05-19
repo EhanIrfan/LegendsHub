f640de11f292050d89c2204341063b751c87b109
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:42:03 -0400

    Update Info.py

--- Info.py
+++ Info.py
@@ -5,9 +5,9 @@ st.set_page_config(
     page_title="DBL"
 )
 
-source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
-print(source_code)
-components.html(source_code, height=600)
+# source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
+# print(source_code)
+# components.html(source_code, height=600)
 
 st.write("DBL so fun")
 
