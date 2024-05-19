69241edd502e76209b531f24fc1f94c7fd94c528
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-05-18 23:39:49 -0400

    Update Randomizer.py

--- pages/Randomizer.py
+++ pages/Randomizer.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Title of page and size
-st.set_page_config(page_title="Legends Randomizer", layout="wide")
-
-
-
 
 source_code = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1269390288736567" crossorigin="anonymous"></script>'
 print(source_code)
