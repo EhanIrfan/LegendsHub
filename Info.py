80b8a662f600991ea9c698c9d1cc09139217f32b
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-11-27 13:20:51 -0500

    Update Info.py

--- Info.py
+++ Info.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_title="DBL"
 )
 
-st.write("Get shafted for free")
+st.write("If there are any bugs email fakehanat@gmail.com")
 
 st.sidebar.success("Select a page above.")
 
