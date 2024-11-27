8ebd9c54bd3f0b3a20df5b1af3d3781dcdf23489
EhanIrfan <157241130+EhanIrfan@users.noreply.github.com>
2024-11-27 14:12:52 -0500

    Update Info.py

--- Info.py
+++ Info.py
@@ -6,6 +6,10 @@ st.set_page_config(
 )
 
 st.write("If there are any bugs email fakehanat@gmail.com")
+st.write("Things to note:")
+st.write("For tag characters/revival characters/transforming characters, only one of the characters/forms will be shown. It is inconsistent, so you shouldn't assume that it will show the pre-transformed version for all characters.")
+st.write("Always trust the DBL tags, they are what will be 100% accurate. The images will be right most of the time as well but may mess up for certain characters.")
+st.write("Some characters are not in this such as shallot, giblet, saiyan in red... All summonable characters are valid.")
 
 st.sidebar.success("Select a page above.")
 
